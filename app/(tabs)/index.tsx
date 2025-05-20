ffcab70d024623269f3f49fa544600ee71b0e43d
SidhartP01 <sidharthpillayil@gmail.com>
2025-05-20 14:53:47 +0530

    Routing completed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-accent font-bold">Welcome</Text>
-      <Link href="/onboarding">onboarding</Link>
     </View>
   );
 }
