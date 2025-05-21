84b6b492d659ef2cf48d0fb800fad27bb81ceb3f
SidhartP01 <sidharthpillayil@gmail.com>
2025-05-21 16:40:28 +0530

    Moviecard Created

---
+++ services/api.ts
@@ -0,0 +1,26 @@
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+        query
+      )}&api_key=${TMDB_CONFIG.API_KEY}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?api_key=${TMDB_CONFIG.API_KEY}&sort_by=popularity.desc`;
+  console.log(endpoint);
+
+  const response = await fetch(endpoint);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch Movies: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
